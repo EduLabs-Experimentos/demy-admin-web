5fcbbb53cd9caa77402676bb3739c42404282d9a
Daniel <u20221g120@upc.edu.pe>
2026-05-06 22:49:14 -0500

    feat: add environment configuration files for development and production

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  platformProviderApiBaseUrl: '',
+  platformProviderSignInEndpointPath: '/sign-in',
+  platformProviderSignUpEndpointPath: '/sign-up',
+};
