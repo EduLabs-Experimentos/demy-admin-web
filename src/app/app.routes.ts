6a1b27dcf5868d94aa0d054dd3832f36763c8fbd
diegovilcasaboya <diegovilcatut@gmail.com>
2026-05-09 21:05:33 -0500

    feat(billing): update app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -29,6 +29,7 @@ export const routes: Routes = [
 
       // 👇 ¡AQUÍ ESTÁ TU RUTA DE ESTUDIANTES! 👇
       { path: 'students', loadChildren: () => import('./students/presentation/students.routes').then(m => m.studentsRoutes) },
+      { path: 'billing', loadChildren:() => import('./billing/presentation/billing.routes').then(m => m.billingRoutes) },
 
       { path: 'organization/courses', loadChildren: () => import('./courses/presentation/courses.routes').then(m => m.coursesRoutes) },
       { path: 'organization/classrooms', loadChildren: () => import('./classrooms/presentation/classrooms.routes').then(m => m.classroomsRoutes) }
